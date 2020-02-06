0927b4ab241a0199c8f1d5fa225a5fc545649405
fdaw22 <fv15astur@gmail.com>
2020-02-06 20:56:19 +0100

    Version 5

--- ANGULAR/Ejercicio24/src/COMPONENTES/BODY/body.component.ts
+++ ANGULAR/Ejercicio24/src/COMPONENTES/BODY/body.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 
+//Declaramos las variables
+declare var jQuery:any;
+declare var $:any;
+
+
 @Component({
   selector: 'app-body',
-  templateUrl: './body.component.html'
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
 })
+
 export class BodyComponent {
   title = 'Ejercicio24';
+
+  public mostrarTitulo(){
+    console.log("click");
+ 
+   // Usamos jQuery con normalidad
+    $("#title").slideToggle();
+  }
+
 }
--- ANGULAR/Ejercicio24/src/app/app.component.ts
+++ ANGULAR/Ejercicio24/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+//Declaramos las variables
+declare var jQuery:any;
+declare var $:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ejercicio24';
+//Preguntar a pablo como hacer para agregar esta funcion al onload de la página
+public crearLista(){
+  alert("Cara polla");
+  var modulos=["Informática", "Administración", "Electricidad"];
+  var j=1;
+  for(var i=0; i<modulos.length;i++){
+    $(".list-group").append("<li class='list-group-item'>"+j+" - "+modulos[i]+"</li>");
+    j++;
+  }
+}
+
 }
