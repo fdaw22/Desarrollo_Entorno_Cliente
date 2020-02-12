e1b80a80f195a0abc71c7a3d33926da574ff7e05
fdaw22 <fv15astur@gmail.com>
2020-02-12 14:27:14 +0100

    Versión 9, con el evento onclick y el evento onload(ngOnInit()) ya funcionales

--- ANGULAR/Ejercicio24/src/COMPONENTES/BODY/body.component.ts
+++ ANGULAR/Ejercicio24/src/COMPONENTES/BODY/body.component.ts
@@ -13,7 +13,15 @@ declare var $:any;
 
 export class BodyComponent {
   title = 'Ejercicio24';
-
+  ngOnInit(){
+    //alert("Cara polla");
+    var modulos=["Informática", "Administración", "Electricidad"];
+    var j=1;
+    for(var i=0; i<modulos.length;i++){
+      $(".list-group").append("<li class='list-group-item'>"+j+" - "+modulos[i]+"</li>");
+      j++;
+    }
+  }
   public mostrarTitulo(){
     console.log("click");
  
--- ANGULAR/Ejercicio24/src/app/app.component.ts
+++ ANGULAR/Ejercicio24/src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component } from '@angular/core';
 
-//Declaramos las variables
-declare var jQuery:any;
-declare var $:any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,14 +8,5 @@ declare var $:any;
 export class AppComponent {
   title = 'Ejercicio24';
 //Preguntar a pablo como hacer para agregar esta funcion al onload de la página
-public crearLista(){
-  alert("Cara polla");
-  var modulos=["Informática", "Administración", "Electricidad"];
-  var j=1;
-  for(var i=0; i<modulos.length;i++){
-    $(".list-group").append("<li class='list-group-item'>"+j+" - "+modulos[i]+"</li>");
-    j++;
-  }
-}
 
 }
