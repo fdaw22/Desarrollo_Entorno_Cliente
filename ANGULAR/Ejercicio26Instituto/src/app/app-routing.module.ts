cd69a691bc76cfbeb0993fa5e2be6f2b64bce5d4
fdaw22 <fv15astur@gmail.com>
2020-02-25 01:31:03 +0100

    Version13

--- ANGULAR/Ejercicio26Instituto/src/app/app-routing.module.ts
+++ ANGULAR/Ejercicio26Instituto/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartamentoComponent } from 'src/COMPONENTES/DEPARTAMENTOS/departamentos.component';
+import { FamiliasComponent } from 'src/COMPONENTES/FAMILIAS/familias.component';
+import { AppComponent } from './app.component';
+import { PaginaNotFoundComponent } from 'src/COMPONENTES/PAGINANOTFOUND/paginanotfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component: AppComponent},
+  {path:'Departamento',component: DepartamentoComponent},
+  {path:'Familias',component: FamiliasComponent},
+  {path:'**', component:PaginaNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ANGULAR/Ejercicio26Instituto/src/app/app.module.ts
+++ ANGULAR/Ejercicio26Instituto/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BuscarComponent } from 'src/COMPONENTES/BUSCAR/buscar.component';
