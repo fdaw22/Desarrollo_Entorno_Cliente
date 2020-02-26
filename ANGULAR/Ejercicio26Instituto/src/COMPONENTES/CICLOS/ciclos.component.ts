e08958cf9be63a5744e7feec6115c3f03a78033e
fdaw22 <fv15astur@gmail.com>
2020-02-27 00:44:41 +0100

    Version17

--- ANGULAR/Ejercicio26Instituto/src/COMPONENTES/CICLOS/ciclos.component.ts
+++ ANGULAR/Ejercicio26Instituto/src/COMPONENTES/CICLOS/ciclos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DepartamentosService } from 'src/app/SERVICIOS/departamentos.service';
 
 @Component({
   selector: 'app-ciclos',
@@ -10,8 +11,24 @@ export class CiclosComponent {
   title = 'Ejercicio26Instituto';
 
   id:any;
-  constructor(private _route: ActivatedRoute){
+  auxiliar:any [];
+  deptnoFinal:any [];
+  ciclosFinal:any[];
+  constructor(private _route: ActivatedRoute, private departamentoServicio: DepartamentosService){
     this.id=this._route.snapshot.paramMap.get('id');
-    alert(this.id);
+    alert(this.departamentoServicio.departamentos);
+    this.auxiliar=this.departamentoServicio.departamentos;
+   
   }
+
+  ngOnInit() {
+    for(var i=0;i<this.auxiliar.length;i++){
+      if(this.auxiliar[i].id==this.id){
+        this.deptnoFinal.push.apply(this.auxiliar[i]);
+        //alert(this.deptnoFinal[i].ciclos);
+      }
+    }
+  }
+
+
 }
