77c5294b61ef1b452ec88e59ade1cebc6995ba37
fdaw22 <fv15astur@gmail.com>
2020-03-12 11:12:07 +0100

    Version29

--- ANGULAR/Ejercicio26Instituto/src/COMPONENTES/FORMULARIO/formulario.component.ts
+++ ANGULAR/Ejercicio26Instituto/src/COMPONENTES/FORMULARIO/formulario.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DepartamentosService } from 'src/app/SERVICIOS/departamentos.service';
+import {Ciclo} from '../../app/MODELOS/ciclo.mode';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-formulario',
@@ -11,4 +14,18 @@ export class FormularioComponent {
   nombre:string="";
   descripcion:string="";
   imagen:string="";
+  dep:any[];
+  identificador:any;
+  ciclo: Ciclo [];
+
+  constructor(private route: ActivatedRoute,private departamentoServicio: DepartamentosService){
+
+    //this.dep= this.departamentoServicio.getDepartamentos(); con metodos normales sin obserbavle
+    //alert(this.dep);
+    this.identificador=this.route.snapshot.paramMap.get('id');
+  }
+
+  ngOnInit(){
+    this.departamentoServicio.getDepartamento(this.identificador).subscribe( dpts => this.ciclo= dpts );//con observable
+  }
 }
