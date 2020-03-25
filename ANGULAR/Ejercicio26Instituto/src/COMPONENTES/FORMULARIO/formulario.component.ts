b0dcc98ff50994e61e1060e953d6a6894fd3cf83
fdaw22 <fv15astur@gmail.com>
2020-03-25 20:56:15 +0100

    Version 32

--- ANGULAR/Ejercicio26Instituto/src/COMPONENTES/FORMULARIO/formulario.component.ts
+++ ANGULAR/Ejercicio26Instituto/src/COMPONENTES/FORMULARIO/formulario.component.ts
@@ -3,6 +3,7 @@ import { DepartamentosService } from 'src/app/SERVICIOS/departamentos.service';
 import {Ciclo} from '../../app/MODELOS/ciclo.mode';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
@@ -10,22 +11,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FormularioComponent {
   title = 'Ejercicio26Instituto';
-  id:string="hola";
+  id:string="";
   nombre:string="";
   descripcion:string="";
   imagen:string="";
-  dep:any[];
+  dep:Ciclo[];
   identificador:any;
   ciclo: Ciclo [];
-
+  cambiador:boolean;
   constructor(private route: ActivatedRoute,private departamentoServicio: DepartamentosService){
 
     //this.dep= this.departamentoServicio.getDepartamentos(); con metodos normales sin obserbavle
     //alert(this.dep);
     this.identificador=this.route.snapshot.paramMap.get('id');
+    alert(this.identificador);
   }
 
   ngOnInit(){
     this.departamentoServicio.getDepartamento(this.identificador).subscribe( dpts => this.ciclo= dpts );//con observable
+  }
+
+ Guardar(){
+    this.departamentoServicio.departamentos.push()
   }
 }
--- ANGULAR/Ejercicio26Instituto/src/COMPONENTES/PAISES/paises.component.ts
+++ ANGULAR/Ejercicio26Instituto/src/COMPONENTES/PAISES/paises.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnDestroy, OnInit} from '@angular/core';
 import { ApiRestService } from 'src/app/SERVICIOS/api-rest.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-paises',
@@ -10,14 +11,21 @@ export class PaisesComponent {
   title = 'Ejercicio26Instituto';
   
   dtOptions: DataTables.Settings = {};
-  pais:any[];
+  pais:any[]=[];
+  dtTrigger: Subject<any> = new Subject();
 
   constructor(private ApiRest:ApiRestService){
   }
   ngOnInit(): void {
-    this.dtOptions = {pagingType: 'full_numbers'};
+    this.dtOptions = {pagingType: 'full_numbers', pageLength: 2};
     this.ApiRest.getAllIdiomas().subscribe(paises => {console.log(paises)});
-    this.ApiRest.getAllIdiomas().subscribe(paises => this.pais=paises);
+    this.ApiRest.getAllIdiomas().subscribe(paises => {this.pais=paises; this.dtTrigger.next();});
     alert(this.pais);
   }
+
+  ngOnDestroy(): void {
+    // Do not forget to unsubscribe the event
+    this.dtTrigger.unsubscribe();
+  }
+
 }
